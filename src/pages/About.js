e1c493e5674dd76162244fd9c7f9bc0624e527d3
KristinaValencak <valencakkristina@gmail.com>
2023-12-12 18:49:13 +0100

    Changes.

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,8 +13,12 @@ function About() {
             <div className="description">
                 <h1>About <span>Me</span></h1>
                 
-                <p>Hey there! 👋 I'm Kristina, a self-taught frontend programmer with a boundless passion for crafting digital experiences. My journey into the world of coding began as a personal quest, driven by an insatiable curiosity and a determination to turn ideas into captivating web interfaces.🚀 As a proactive individual, I thrive on the dynamic nature of frontend development, where each line of code brings creativity to life. I specialize in translating design concepts into interactive, user-friendly websites that leave a lasting impression.
-                  💡 What fuels my enthusiasm? The endless possibilities within the ever-evolving tech landscape! I find joy in staying ahead of the curve, learning new technologies, and embracing challenges head-on. With a commitment to continuous growth, I strive to elevate user experiences through elegant and efficient code. Let's embark on this coding journey together, where innovation meets passion! 💻✨
+                <p>Hey there! 👋 I'm Kristina, a self-taught frontend programmer with a boundless passion for crafting digital experiences. 
+                  My journey into the world of coding began as a personal quest, driven by an insatiable curiosity and a determination to turn ideas into captivating web interfaces.
+                  🚀 As a proactive individual, I thrive on the dynamic nature of frontend development, where each line of code brings creativity to life.
+                  I specialize in translating design concepts into interactive, user-friendly websites that leave a lasting impression.💡 What fuels my enthusiasm? The endless possibilities within
+                   the ever-evolving tech landscape! I find joy in staying ahead of the curve, learning new technologies, and embracing challenges head-on. 
+                   With a commitment to continuous growth, I strive to elevate user experiences through elegant and efficient code. Let's embark on this coding journey together, where innovation meets passion!💻✨
             </p>
                 
             </div>
